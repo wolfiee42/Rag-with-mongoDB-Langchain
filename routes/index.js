7be9aff38d32f66f07d068dadcdc82635f2053ec
wolfiee42 <saifalislam2022@gmail.com>
2025-02-10 10:25:56 +0600

    resolved conflict and removed some garbage code.

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,6 @@ import { uploadPDFToMulterMemory } from "../utils/multerStorage.js";
 
 const router = Router();
 
-router.get("/init", controller.method1);
-// * upload pdf with multer ans aws s3 to r2, after successful  upload, return the key
-router.post(
-  "/upload-pdf",
-  uploadPDFToMulterMemory.single("file"),
-  controller.r2PDFUpload
-);
-// * get pdf from r2 by key (returned by previous endpoint)
-router.get("/get-pdf/:key", controller.r2PDFGet);
-
 router.post(
   "/upload-pdf-to-r2",
   uploadPDFToMulterMemory.single("file"),
